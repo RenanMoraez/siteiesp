2a46f34d1b5aeb07cbb3821ddb970d00b2ca38a9
Renan Moraes <renanmoraesaraujo@gmail.com>
2020-05-14 12:51:42 -0300

    Incluindo a pasta do projeto
    Signed-off-by: Renan Moraes <renanmoraesaraujo@gmail.com>

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IESP: Novo Site</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+<body>
+<!-- Div Container -->
+<div class="sobretopo">
+	
+</div>
+
+<!-- Cabeçalho -->
+<div class="cabecalho">
+	<div class="container-fluid logo">
+		<a href="#"><img src="assets/images/logo.png" class="logo-direito"></a>
+		<a href="http://www.segup.pa.gov.br"><img src="assets/images/logopara.png" class="logo-esquerdo"></a>
+	</div>
+
+</div>
+
+<!-- menu -->
+<div class="barramenu">
+<nav>
+
+                    <ul>
+                    	<li><a href=""><i class="fas fa-home"></i></a></li>
+                        <li><a href="#">INÍCIO</a></li>
+                        <li><a href="#">O GOVERNO</a></li>
+                        <li><a href="#">SERVIÇOS</a></li>
+                        <li><a href="#">AGÊNCIA DE NOTÍCIAS</a></li>
+                        <li><a href="#">PROJETOS E A&Ccedil;&Otilde;ES</a></li>
+
+                        <li></li>
+                    </ul>
+</nav>
+</div>
+
+
+<!-- Slide grande -->
+
+<section>
+	<div class="row">
+		<div class="col">
+			<div class="cor01"></div>
+		</div>		
+	</div>
+</section>
+
+
+<!-- Noticiais -->
+<section class="noticiais">
+	<div class="container">
+		<h1><i class="far fa-newspaper"></i>  Notícias</h1>
+		<hr>
+	</div>
+
+ 	<div class="banner_links">
+			<div class="container row quadros">
+				<div class="col-sm-4">
+					<div class="quadro01"></div>
+				</div>
+				<div class="col-sm-4">
+					<div class="quadro02"></div>
+				</div>
+				<div class="col-sm-4">
+					<div class="quadro03"></div>
+				</div>
+
+				<button class="btn btn-primary botao_noticia">VEJA MAIS +</button>
+			</div>
+		</div>
+</section>
+
+
+<!-- Galerias -->
+<section>
+
+
+
+
+
+
+
+</section>
+
+
+
+
+<!-- rotape -->
+<footer class="rodape">
+
+
+
+<div id="chat"></div>
+</footer>
+<script src='http://186.228.21.253:8082/chattsj/app/disquedenuncia.js' type="text/javascript"></script>
+<script type="text/javascript" href="/assets/js/bootstrap.bundle.js"></script>
+<script type="text/javascript" href="/assets/js/bootstrap.js"></script>
+</body>
+</html>
