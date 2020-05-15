57f912247958d0902e363b0f0b9d3ae640a6b580
Renan Moraez <renanmoraesaraujo.com>
2020-05-14 21:36:13 -0300

    Atualizando o Index do Site
    Signed-off-by: Renan Moraez <renanmoraesaraujo.com>

--- index.php
+++ index.php
@@ -51,23 +51,34 @@
 </section>
 
 
+<!-- banner pequeno largo -->
+<section>
+	<div class="container banner_pequeno">
+		<img src="assets/images/banner.png">
+	</div>
+</section>
+
+
+
+
+
 <!-- Noticiais -->
 <section class="noticiais">
-	<div class="container">
+	<div class="container noticia_letra">
 		<h1><i class="far fa-newspaper"></i>  Notícias</h1>
 		<hr>
 	</div>
 
- 	<div class="banner_links">
+ 	<div class="banner_noticia">
 			<div class="container row quadros">
 				<div class="col-sm-4">
-					<div class="quadro01"></div>
+					<div class="quadro01 shadow-lg bg-light"></div>
 				</div>
 				<div class="col-sm-4">
-					<div class="quadro02"></div>
+					<div class="quadro02 shadow-lg bg-light"></div>
 				</div>
 				<div class="col-sm-4">
-					<div class="quadro03"></div>
+					<div class="quadro03 shadow-lg bg-light"></div>
 				</div>
 
 				<button class="btn btn-primary botao_noticia">VEJA MAIS +</button>
@@ -76,24 +87,80 @@
 </section>
 
 
-<!-- Galerias -->
+<!-- Links -->
 <section>
 
+<div class="container">
+</div>
+<div class="links">
+	<!-- 3 seletores topo-->
+	<div class="row">
+		<div class="col">
+		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px;">
+		  <div class="card-header"></div>
+		  <div class="card-body">
+		    <h5 class="card-title">Biblioteca</h5>
+		    <p class="card-text">Biblioteca</p>
+		 </div>
+		 </div>
+		 </div>
+		 		<div class="col">
+		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
+		  <div class="card-header"></div>
+		  <div class="card-body">
+		    <h5 class="card-title">EAD</h5>
+		    <p class="card-text">Acesse o EAD</p>
+		 </div>
+		 </div>
+		 </div>
+		 		<div class="col">
+		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
+		  <div class="card-header"></div>
+		  <div class="card-body">
+		    <h5 class="card-title">Cadastro de Docente</h5>
+		    <p class="card-text">Biblioteca</p>
+		 </div>
+		 </div>
+		 </div>
+	</div>
 
-
-
-
-
+	<!-- 3 seletores Bottom -->
+	<div class="row">
+		<div class="col">
+		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px;">
+		  <div class="card-header"></div>
+		  <div class="card-body">
+		    <h5 class="card-title">Certificados</h5>
+		    <p class="card-text">Verifique o seu certificado</p>
+		 </div>
+		 </div>
+		 </div>
+		 		<div class="col">
+		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
+		  <div class="card-header"></div>
+		  <div class="card-body">
+		    <h5 class="card-title">Biblioteca</h5>
+		    <p class="card-text">Biblioteca</p>
+		 </div>
+		 </div>
+		 </div>
+		 		<div class="col">
+		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
+		  <div class="card-header"></div>
+		  <div class="card-body">
+		    <h5 class="card-title">Biblioteca</h5>
+		    <p class="card-text">Biblioteca</p>
+		 </div>
+		 </div>
+		 </div>
+	</div>
+</div>
 
 </section>
 
 
-
-
 <!-- rotape -->
 <footer class="rodape">
-
-
 
 <div id="chat"></div>
 </footer>
