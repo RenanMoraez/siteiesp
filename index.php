0b03a4b72432eee377844a54c599d63ddfefc4e5
Renan Moraez <renanmoraesaraujo@gmail.com>
2020-05-22 01:10:34 -0300

    incluindo links e adicionando o plugin do facebook
    Signed-off-by: Renan Moraez <renanmoraesaraujo@gmail.com>

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
                         <li><a href="#">INÍCIO</a></li>
                         <li><a href="#">INSTITUCIONAL</a></li>
                         <li><a href="#">NORMAS</a></li>
+                        <li><a href="#">CURSOS</a></li>
                         <li><a href="#">FALE CONOSCO</a></li>
                         <li><a href="#">PROJETOS E A&Ccedil;&Otilde;ES</a></li>
 
@@ -108,8 +109,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px;">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Biblioteca</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="fas fa-book-reader"></i><br>Biblioteca</h3>
+		    <p class="card-text">Acesse a nossa biblioteca</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -117,8 +121,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">EAD</h5>
-		    <p class="card-text">Acesse o EAD</p>
+		    <h3 class="card-title"><i class="fas fa-graduation-cap"></i><br>Portal de Ensino</h3>
+		    <p class="card-text">Acesse o EAD IESP</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -126,8 +133,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Cadastro de Docente</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="fas fa-chalkboard-teacher"></i><br>Credenciamento de Docentes</h3>
+		    <!-- <p class="card-text">Cadastramento Docente IESP</p> -->
+		 	<div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -139,8 +149,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px;">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Certificados</h5>
+		    <h3 class="card-title"><i class="fas fa-check-circle"></i><br>Certificados</h3>
 		    <p class="card-text">Verifique o seu certificado</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -148,8 +161,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Biblioteca</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="far fa-envelope"></i><br>E-Mail Institucional</h3>
+		    <p class="card-text">Acesse o Zimbra</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -157,8 +173,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Biblioteca</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="fas fa-bullhorn"></i><br>Boletim Informativo</h3>
+		    <p class="card-text"></p>
+		 	<div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -176,19 +195,18 @@
 </section>
 
 
-
+<div id="chat"></div>
 <!-- rotape -->
 <footer class="rodape">
 <div class="left">
 	<img src="assets/images/logo_marca_horizontal_branca.png">	
 </div>
-
 <div class="right">
 	<h3>INSTITUTO DE ENSINO <br> DE SEGURANÇA PÚBLICA <br> DO ESTADO DO PARÁ</h3>
+	<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIesp2018%2F&tabs=eventos&width=340&height=154&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="154" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 </div>
-
-<div id="chat"></div>
 </footer>
+
 <script src="http://186.228.21.253:8082/chattsj/app/disquedenuncia.js" type="text/javascript"></script>
 <script type="text/javascript" href="/assets/js/bootstrap.bundle.js"></script>
 <script type="text/javascript" href="/assets/js/bootstrap.js"></script>
