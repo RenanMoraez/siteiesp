0969185b81c8d1d7a7dd40cb9e52b070c616b5ce
Renan Moraez <renanmoraesaraujo.com>
2020-05-23 23:09:59 -0300

    repositorio
    Signed-off-by: Renan Moraez <renanmoraesaraujo.com>

--- index.php
+++ index.php
@@ -32,8 +32,13 @@
                         <li><a href="#">INÍCIO</a></li>
                         <li><a href="#">INSTITUCIONAL</a></li>
                         <li><a href="#">NORMAS</a></li>
+                        <li><a href="#">CURSOS</a></li>
                         <li><a href="#">FALE CONOSCO</a></li>
-                        <li><a href="#">PROJETOS E A&Ccedil;&Otilde;ES</a></li>
+						<li><a href="#">PROJETOS E A&Ccedil;&Otilde;ES</a></li>
+						<!-- <li><form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+						<button class="btn btn-success my-sm-0" type="submit">Pesquisar</button>
+						</form></li> -->
 
                         <li></li>
                     </ul>
@@ -44,11 +49,41 @@
 <!-- Slide grande -->
 
 
-<section>
+<div class="container-fluid">
 
-	<div class="cor01"></div>
+<div id="carroseltopo" class="carousel slide"  data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carroseltopo" data-slide-to="0" class="active"></li>
+		<li data-target="#carroseltopo" data-slide-to="1"></li>
+		<li data-target="#carroseltopo" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img class="" src="assets/images/festa.jpg" alt="primeiro slide">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Aqui vai</h5>
+				<p>Olha o bombeiro</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="" src="assets/images/bombeiros.jpg" alt="segundo slide">
+		</div>
+		<div class="carousel-item">
+			<img class="" src="assets/images/bombeiros.jpg" alt="terceiro slide">
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#carroseltopo" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" arial-hidden="true"></span>
+		<span class="sr-only">Anterior</span>
+	</a>
+	<a class="carousel-control-next" href="#carroseltopo" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Próximo</span>
+	</a>
+</div>
+	
 
-</section>
+</div>
 
 
 <!-- banner pequeno largo -->
@@ -66,33 +101,54 @@
 <section class="noticiais">
 	<div class="container noticia_letra">
 		<h1><i class="far fa-newspaper"></i>  Notícias</h1>
-		<hr>
 	</div>
 
  	<div class="banner_noticia">
-			<div class="container row quadros">
-				<div class="col-sm-4">
-					<div class="quadro01 shadow-lg bg-light">
-					
-					<p class="legenda_noticia">
-						IESP RECEBE VISITA DO CAP BM HELIO ALMEIDA E DO CB BM LEONARDO.</p>
-					
-					</div>
+		 <div class="row colunas-not">
+			 <!-- primeira coluna -->
+			 <div class="col-sm-4">
+				<div class="card">
+				<img class="card-img-top" src="assets/images/imagem.png" alt="">
+						<div class="card-body2">
+						 	<div class="card-title">
+								<h4>Lorem Ipsum</h4>
+								<p class="card-text">
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magnam unde fugit incidunt reprehenderit commodi? Sapiente non maxime at quo recusandae ex optio labore perspiciatis corporis quisquam, nostrum earum laudantium.
+								</p>
+							</div>
+						</div>
 				</div>
-				<div class="col-sm-4">
-					<div class="quadro02 shadow-lg bg-light">
-					<p class="legenda_noticia">IESP REALIZA REUNIÃO COM EQUIPE DO DETRAN PARA AJUSTES DO CURSO DE FORMAÇÃO DE AGENTES DE TRÂNSITO</p>
-					</div>
+			</div>
+			<!-- segunda coluna -->
+			<div class="col-sm-4">
+				<div class="card">
+					<img class="card-img-top" src="assets/images/imagem.png" alt="">
+						<div class="card-body2">
+							<div class="card-title">
+								<h4>Lorem Ipsum</h4>
+								<p class="card-text">
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magnam unde fugit incidunt reprehenderit commodi? Sapiente non maxime at quo recusandae ex optio labore perspiciatis corporis quisquam, nostrum earum laudantium.
+								</p>
+							</div>
+						</div>
 				</div>
-				<div class="col-sm-4">
-					<div class="quadro03 shadow-lg bg-light">
-					<p class="legenda_noticia">OUVIDORIA DO SIEDS REALIZA VISITA AO IESP</p>
-					</div>
+			</div>
+			<!-- terceira coluna -->
+			<div class="col-sm-4">
+				<div class="card">
+					<img class="card-img-top" src="assets/images/imagem.png" alt="">
+						<div class="card-body2">
+							<div class="card-title">
+								<h4>Lorem Ipsum</h4>
+								<p class="card-text">
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magnam unde fugit incidunt reprehenderit commodi? Sapiente non maxime at quo recusandae ex optio labore perspiciatis corporis quisquam, nostrum earum laudantium.
+								</p>
+							</div>
+						</div>
 				</div>
-
-				<button class="btn btn-primary botao_noticia">VEJA MAIS +</button>
 			</div>
 		</div>
+	</div>
 </section>
 
 
@@ -108,26 +164,35 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px;">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Biblioteca</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="fas fa-book-reader"></i><br>Biblioteca</h3>
+		    <p class="card-text">Acesse a nossa biblioteca</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
-		 		<div class="col">
+		<div class="col">
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">EAD</h5>
-		    <p class="card-text">Acesse o EAD</p>
+		    <h3 class="card-title"><i class="fas fa-graduation-cap"></i><br>Portal de Ensino</h3>
+		    <p class="card-text">Acesse o EAD IESP</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
-		 		<div class="col">
+		<div class="col">
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Cadastro de Docente</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="fas fa-chalkboard-teacher"></i><br>Credenciamento de Docentes</h3>
+		    <!-- <p class="card-text">Cadastramento Docente IESP</p> -->
+		 	<div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -139,8 +204,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px;">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Certificados</h5>
+		    <h3 class="card-title"><i class="fas fa-check-circle"></i><br>Certificados</h3>
 		    <p class="card-text">Verifique o seu certificado</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -148,8 +216,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Biblioteca</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="far fa-envelope"></i><br>E-Mail Institucional</h3>
+		    <p class="card-text">Acesse o Zimbra</p>
+		    <div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -157,8 +228,11 @@
 		<div class="card text-white bg-dark" style="max-width: 20rem; margin-top: 40px; ">
 		  <div class="card-header"></div>
 		  <div class="card-body">
-		    <h5 class="card-title">Biblioteca</h5>
-		    <p class="card-text">Biblioteca</p>
+		    <h3 class="card-title"><i class="fas fa-bullhorn"></i><br>Boletim Informativo</h3>
+		    <p class="card-text"></p>
+		 	<div class="botao-bloco">
+		    <a href="#" role="button" class="btn btn-access">Acessar</a>
+		 	</div>
 		 </div>
 		 </div>
 		 </div>
@@ -170,27 +244,27 @@
 <section>
 <div class="container noticia_letra">
 		<h1><i class="fas fa-camera-retro"></i>  Galeria</h1>
-		<hr>
 	</div>
 
 </section>
 
 
-
+<div id="chat"></div>
 <!-- rotape -->
 <footer class="rodape">
 <div class="left">
 	<img src="assets/images/logo_marca_horizontal_branca.png">	
 </div>
-
 <div class="right">
 	<h3>INSTITUTO DE ENSINO <br> DE SEGURANÇA PÚBLICA <br> DO ESTADO DO PARÁ</h3>
+	<!-- IESP Iframe -->
+	<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIesp2018%2F&tabs=eventos&width=340&height=154&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="154" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 </div>
 
-<div id="chat"></div>
 </footer>
+
 <script src="http://186.228.21.253:8082/chattsj/app/disquedenuncia.js" type="text/javascript"></script>
-<script type="text/javascript" href="/assets/js/bootstrap.bundle.js"></script>
-<script type="text/javascript" href="/assets/js/bootstrap.js"></script>
+<script type="text/javascript" href="assets/js/bootstrap.bundle.js"></script>
+<script type="text/javascript" href="assets/js/bootstrap.js"></script>
 </body>
 </html>
